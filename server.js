922a37cc65d5d21fbe81030d61d856663cffee57
ObjectIsAdvantag <steve.sfartz@gmail.com>
2017-09-04 18:43:30 +0200

    added support for Heroku session affinity

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var http = require('http'),
     connect = require('connect'),
     httpProxy = require('http-proxy'),
     bodyParser = require('body-parser'),
+    cookieParser = require("cookie"),
     assert = require('assert');
 
 var debug = require("debug")("injector");
@@ -39,6 +40,10 @@ var proxyOptions = {
     // Check SSL at target
     secure: true
 };
+
+
+// Used to filter out affinity if it's the bot who generated the notification
+var botPersonId = process.env.BOT_PERSONID;
 
 var app = connect()
     .use(bodyParser.json())
@@ -67,7 +72,7 @@ var app = connect()
         proxy.web(req, res, proxyOptions);
     });
 
-var proxyPort = process.env.PORT || 9090;    
+var proxyPort = process.env.PORT || 9090;
 http.createServer(app).listen(proxyPort, function () {
     console.log('listening at: http://localhost:' + proxyPort);
     console.log('proxying to: ' + proxyOptions.target);
@@ -76,26 +81,82 @@ http.createServer(app).listen(proxyPort, function () {
 // Invoked when the connection is established to the target
 proxy.on('proxyReq', function (proxyReq, req, res, options) {
 
-    // if POST request, add ActorId HTTP header
+    // if POST request, check actorId from HTTP header
     if (req.method == 'POST') {
         assert(req.body !== undefined);
 
-        // Inject ActorId
-        if (req.body.actorId) {
-            fine("actorId found: " + req.body.actorId);
+        // [TODO] Add option to check secret
 
+        // Check actorId is present, and our bot is not the actor
+        var actorId = req.body.actorId;
+        if (!actorId && (actorId == botPersonId)) {
+            // No affinity to setup for incoming request
+            if (actorId) {
+                fine("no affinity task: our bot is the actor");                
+            }
+            else {
+                fine("no affinity task: actorId not present in incoming request");
+            }
+        }
+        else {
             // Inject ActorId
-            proxyReq.setHeader('ActorId', req.body.actorId);
+            fine("injecting 'ActorId' HTTP header: " + actorId)
+            proxyReq.setHeader('ActorId', actorId);
+
+            // Is there an affinity registered for the Spark user
+            var cookie = this.fetchCookie(actorId);
+            if (cookie) {
+                fine("injecting 'heroku-session-affinity' cookie for actor: " + actorId);
+                proxyReq.setHeader('cookie', "heroku-session-affinity=" + cookie['heroku-session-affinity']);
+            }
+            else {
+                fine("no cookie found for actorId: " + actorId);                
+            }
         }
     }
 
     // Restream body if contents got parsed by JSON body parser
     if (req.body) {
+        // [TODO] Watchout: we are certainly not altering the secret !!! 
+        // Better memorize raw data and rewrite those
+
         var bodyData = JSON.stringify(req.body);
-        // Override content length as we have de/reserialzed the body
+        // Override content length as we have de/reserialized the body
         proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
         proxyReq.write(bodyData);
     }
+});
+
+// Invoked when the response is received from the target
+proxy.on('proxyRes', function (proxyRes, req, res) {
+
+    // Is there an actorId to associate the cookie to
+    var actorId = req.body.actorId;
+    if (!actorId) {
+        fine("no actorId in current request => no affinity to setup");
+        return;
+    }
+
+    // Looking for Cookie Affinity setup
+    // - example: heroku-session-affinity=ACyDaANoA24IARCYXdj///8HYgAIEpdiAA0DpmEBbAAAAAFtAAAABXdlYi4xaqGFOCQUx+sAz805r29nOj/0jxJ5; Version=1; Expires=Tue, 05-Sep-2017 12:44:07 GMT; Max-Age=86400; Domain=webhook-affinity-bot.herokuapp.com; Path=/
+    var newCookiesHeader = proxyRes.headers["set-cookie"];
+    if (!newCookiesHeader) {
+        fine("no affinity cookie to store for actorId: " + actorId);
+        return;
+    }
+
+    // Trying to associate cookie to current actorId
+    var self = this;
+    newCookiesHeader.forEach(function (cookieHeader) {
+        var cookie = cookieParser.parse(cookieHeader);
+        if (cookie['heroku-session-affinity']) {
+            fine("found set-cookie 'heroku-session-affinity'");
+
+            // Associate cookie to request ActorId
+            debug("storing 'heroku-session-affinity' cookie for actorId: " + actorId);
+            self.storeCookie(actorId, cookie)
+        }
+    });
 });
 
 // Inovked if the incoming or outgoing connection break or time out
@@ -122,3 +183,21 @@ proxy.on('error', function (err, req, res) {
     var message = JSON.stringify({ status: 500, description: "Internal Server Error", error: err.message });
     res.end(message);
 });
+
+
+//
+//
+//
+
+// Hashmap of actorId / cookies
+proxy.storage = {};
+proxy.storeCookie = function (actorId, cookie) {
+    this.storage[actorId] = cookie;
+}
+
+proxy.fetchCookie = function (actorId) {
+    return this.storage[actorId];
+}
+
+
+
